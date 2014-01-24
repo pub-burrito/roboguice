c488c50ddee7b23e2f362da0cea7e450b9c134ed
Steven Berlanga <steven.berlanga@cloud.com>
2014-01-24 10:22:40 -0500

    Added url fetching method in property loader

--- roboguice/src/main/java/roboguice/base/util/PropertyLoader.java
+++ roboguice/src/main/java/roboguice/base/util/PropertyLoader.java
@@ -3,6 +3,7 @@ package roboguice.base.util;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Enumeration;
@@ -13,6 +14,34 @@ import roboguice.base.util.logging.Ln;
 
 public class PropertyLoader {
 
+    /**
+     * Returns a list of {@link URL}s return from {@link ClassLoader#getResources(String)}
+     * sorted by the comparator given. If no comparator is given, order is not guaranteed.
+     * 
+     * @param resource The path of the resource to look for
+     * @param comparator The comparator to sort the resources by
+     * @return A list of URLs sorted by the given comparator
+     */
+    public static List<URL> urlsFor( String resource, Comparator<URL> comparator )
+    {
+        List<URL> allUrls = new ArrayList<URL>();
+        
+        try {
+            Enumeration<URL> urls = PropertyLoader.class.getClassLoader().getResources( resource );
+            allUrls = Collections.list(urls);
+            
+            if ( comparator != null )
+            {
+                Collections.sort(allUrls, comparator);
+            }
+            
+            } catch (IOException e) {
+                Ln.e( e );
+            }
+        
+        return allUrls;
+    }
+    
     /**
      * Loads a property file into specific ( or new ) {@link Properties} object
      * 
@@ -25,52 +54,40 @@ public class PropertyLoader {
         boolean set = false;
         Properties tmp = property == null ? new Properties() : property;
 
-        try 
-        {
-            Enumeration<URL> urls = PropertyLoader.class.getClassLoader().getResources( propertyFile );
-            List<URL> allUrls = Collections.list(urls);
-            if ( comparator != null )
-            {
-                Collections.sort(allUrls, comparator);
-            }
-        
-            for( URL url : allUrls)
-            {//for each url
+        List<URL> allUrls = urlsFor( propertyFile, comparator );
+    
+        for( URL url : allUrls)
+        {//for each url
+            
+            InputStream in = null;
+            try 
+            {//and load the property file
+                in = url.openStream();
                 
-                InputStream in = null;
+                if ( in != null )
+                {
+                    tmp.load( in );
+                    set = true;
+                } 
+                else 
+                {
+                    Ln.w( "Could not find [%s] resource - can not inject any resources in specified file.", propertyFile );
+                }
+            } 
+            catch (Exception e) 
+            {
+                Ln.e( e, "Error loading property file [%s]", propertyFile );
+            } 
+            finally 
+            {
                 try 
-                {//and load the property file
-                    in = url.openStream();
-                    
-                    if ( in != null )
-                    {
-                        tmp.load( in );
-                        set = true;
-                    } 
-                    else 
-                    {
-                        Ln.w( "Could not find [%s] resource - can not inject any resources in specified file.", propertyFile );
-                    }
-                } 
-                catch (Exception e) 
                 {
-                    Ln.e( e, "Error loading property file [%s]", propertyFile );
-                } 
-                finally 
+                    in.close();
+                } catch (Exception ex) 
                 {
-                    try 
-                    {
-                        in.close();
-                    } catch (Exception ex) 
-                    {
-                        // ignore
-                    }
+                    // ignore
                 }
             }
-        }
-        catch ( IOException e1 ) 
-        {
-            Ln.e(e1);
         }
             
         return set ? tmp : null;
